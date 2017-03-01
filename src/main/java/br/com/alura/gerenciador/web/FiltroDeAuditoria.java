d451b5852c0601dd60df523ba757ca8d8bb8b6ee
usr-1048 <usr-1048@usr-1048-PC.nt.embratel.com.br>
2017-03-01 14:37:02 -0300

    Filter improvement

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,28 +9,46 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-@WebFilter(urlPatterns="/*")
-public class FiltroDeAuditoria implements Filter{
+@WebFilter(urlPatterns = "/*")
+public class FiltroDeAuditoria implements Filter {
 
 	@Override
 	public void destroy() {
-		
+
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest  req = (HttpServletRequest)request;
-		System.out.println("Usuário acessando a URI " + req.getRequestURI());
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uri = req.getRequestURI();
+		Cookie cookie = getUsuario(req);
+		if(cookie != null) {
+			System.out.println("Usuário " + cookie.getValue() + " acessando a URI " + uri);
+		} else {
+			System.out.println("Usuário <deslogado> acessando a URI " + uri);
+		}
 		chain.doFilter(request, response);
+
+	}
+
+	private Cookie getUsuario(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		if(cookies == null) return null;
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("usuario.logado")) {
+				return cookie;
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		
+
 	}
 
 }
