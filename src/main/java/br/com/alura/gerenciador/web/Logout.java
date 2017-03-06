415913ad11c32018b3553e4df42cfe8fbbf7b070
usr-1048 <usr-1048@usr-1048-PC.nt.embratel.com.br>
2017-03-06 16:46:43 -0300

    Remoção do diretório "paginas" e criação do diretório "view"

--- src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -8,15 +8,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = "/logout")
 public class Logout extends HttpServlet {
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getSession().removeAttribute("usuario.logado");
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
-		dispatcher.forward(req, resp);
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	        throws ServletException, IOException {
+
+	    HttpSession session = req.getSession();
+	    session.removeAttribute("usuario.logado");
+
+	    RequestDispatcher dispatcher = req
+	            .getRequestDispatcher("/WEB-INF/view/logout.html");
+	    dispatcher.forward(req, resp);
+
 	}
 
 }
